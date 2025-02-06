0e8c747001015b02589ee9846395dd098d5d5d6b
Amiu <110311878+AmiuLittle@users.noreply.github.com>
2025-02-06 14:44:39 -0500

    Pre-Alpha 0.1.1
    Moved matrix math to its own cpp file
    Added inputState to tick functions
    Added camera scene element
    Added camera movement with the cpad or dpad

--- amius_adventure/src/rotateCube.hpp
+++ amius_adventure/src/rotateCube.hpp
@@ -2,7 +2,7 @@
 #ifndef LIB_ROTATE_CUBE
 #include "amius_adventure.hpp"
 
-void rotateCubeTick(AmiusAdventure::Scene::Object*, AmiusAdventure::Scene::SceneCtx*);
+void rotateCubeTick(AmiusAdventure::Scene::Object*, AmiusAdventure::Scene::SceneCtx*, AmiusAdventure::Input::InputState*);
 
 #define LIB_ROTATE_CUBE
 #endif

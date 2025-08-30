96b22b75927d70acaea4446c55de33d7acd84d4b
Amiu <110311878+AmiuLittle@users.noreply.github.com>
2025-08-30 13:25:32 -0400

    Update to not include SDL and SDL_mixer as my code

--- getLinesOfCode.sh
+++ getLinesOfCode.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-files_that_count=$(find . -type f \( -name "*.cpp" -o -name "*.hpp" -o -name "*.glsl.vert" -o -name "*.glsl.frag" -o -name "*.v.pica" \) \( -not -path "./include/*" -a -not -path "./simdjson/*" \))
+files_that_count=$(find . -type f \( -name "*.cpp" -o -name "*.hpp" -o -name "*.glsl.vert" -o -name "*.glsl.frag" -o -name "*.v.pica" \) \( -not -path "./include/*" -a -not -path "./simdjson/*" -a -not -path "./SDL/*" -a -not -path "./SDL_mixer/*" \))
 
 lines=0
 
@@ -9,4 +9,3 @@ while IFS= read -r file; do
 done <<< "$files_that_count"
 
 echo $lines
-echo $files_that_count
